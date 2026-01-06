23ba923a91faa7387372af0595a06f6845a8baf0
Izeno <78334105+VoxDroid@users.noreply.github.com>
2026-01-06 22:41:17 +0900

    v0.1.0

--- internal/version/version.go
+++ internal/version/version.go
@@ -2,4 +2,4 @@ package version
 
 // Version is set at build time via -ldflags "-X github.com/VoxDroid/krnr/internal/version.Version=<value>"
 // The default is a development placeholder.
-var Version = "v0.0.0-dev"
+var Version = "v0.1.0"
